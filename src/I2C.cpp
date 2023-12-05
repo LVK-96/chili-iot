ec46bd2dd82abccacb497c94dbe4aa434c3b2e88
Leo Kivikunnas <leo@kivikunnas.xyz>
2023-12-05 23:13:10 +0200

    I2C code cleaning

--- src/I2C.cpp
+++ src/I2C.cpp
@@ -7,7 +7,7 @@ void I2C::setup() const
 {
     I2C_CR1(i2c_dev) &= ~I2C_CR1_STOP; // Clear stop
     i2c_set_speed(i2c_dev, i2c_speed_sm_100k, rcc_apb1_frequency / 1e6);
-    i2c_set_own_7bit_slave_address(I2C2, 0xff); // Why is this necessary???
+    i2c_set_own_7bit_slave_address(i2c_dev, 0xff); // Why is this necessary???
 }
 
 void I2C::enable() const { i2c_peripheral_enable(i2c_dev); }
@@ -16,13 +16,13 @@ void I2C::disable() const { i2c_peripheral_disable(i2c_dev); }
 
 sensor_node_system::ErrorCode I2C::write(uint8_t addr, std::span<const uint8_t> data) const
 {
-    i2c_transfer7(i2c_dev, addr, (const uint8_t*)&data[0], data.size(), nullptr, 0);
+    i2c_transfer7(i2c_dev, addr, data.data(), data.size(), nullptr, 0);
     return sensor_node_system::ErrorCode::OK;
 }
 
 sensor_node_system::ErrorCode I2C::read(uint8_t addr, std::span<uint8_t> data) const
 {
-    i2c_transfer7(i2c_dev, addr, nullptr, 0, &data[0], data.size());
+    i2c_transfer7(i2c_dev, addr, nullptr, 0, data.data(), data.size());
     return sensor_node_system::ErrorCode::OK;
 }
 
