d1f0f1b920e7991617e8ed332ce42daa058752d4
Leo Kivikunnas <leo@kivikunnas.xyz>
2025-11-29 11:03:52 +0200

    Move all GPIO mock tests to test_gpio.cpp

--- tests/test_blinky_led.cpp
+++ tests/test_blinky_led.cpp
@@ -31,27 +31,3 @@ TEST_CASE("GPIOLED construction and interface")
         CHECK(true);
     }
 }
-
-TEST_CASE("GPIOPort operations")
-{
-    GPIOPort port(BluePillGPIOPort::A, RCC_GPIOA, RST_GPIOA);
-
-    SUBCASE("Port operations don't crash")
-    {
-        port.set_pins(GPIO0 | GPIO1);
-        port.clear_pins(GPIO0);
-        port.toggle_pins(GPIO1);
-        uint16_t pins = port.get_pins(GPIO0 | GPIO1);
-        (void)pins; // Suppress unused warning
-
-        CHECK(true);
-    }
-
-    SUBCASE("Port setup operations work")
-    {
-        port.setup_pins(GPIO0, GPIOMode::OUTPUT_2_MHZ, GPIOFunction::OUTPUT_PUSHPULL);
-        port.setup_pins(GPIO1, GPIOMode::INPUT, GPIOFunction::INPUT_FLOAT);
-
-        CHECK(true);
-    }
-}
