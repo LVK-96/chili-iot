1d70192d0ec9cfdf16e904e5cedd168165edfe08
Leo Kivikunnas <leo@kivikunnas.xyz>
2025-11-29 11:03:15 +0200

    Disable shift count overflow warning from bitmask generation utils
    We are allowed to left shift such that the mask is all 1s.

--- src/utils.h
+++ src/utils.h
@@ -6,23 +6,26 @@ class USARTLogger;
 
 namespace utils {
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshift-count-overflow"
 template <unsigned int bits> constexpr inline uint32_t mask32()
 {
     static_assert(bits <= 32);
-    return ((1 << bits) - (uint32_t)1);
+    return ((((uint32_t)1 << bits) - (uint32_t)1));
 }
 
 template <unsigned int bits> constexpr inline uint16_t mask16()
 {
     static_assert(bits <= 16);
-    return ((1 << bits) - (uint16_t)1);
+    return (((uint16_t)1 << bits) - (uint16_t)1);
 }
 
 template <unsigned int bits> constexpr inline uint8_t mask8()
 {
     static_assert(bits <= 8);
-    return ((1 << bits) - (uint8_t)1);
+    return (((uint8_t)1 << bits) - (uint8_t)1); //
 }
+#pragma GCC diagnostic pop
 
 enum class ErrorCode : uint8_t {
     OK = 0,
